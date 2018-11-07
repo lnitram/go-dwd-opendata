7297805945f21173c8d3e4d73e449790ac688c56
Martin Leuschner <martinleuschner@googlemail.com>
2018-11-07 23:04:32 +0100

    more cleanup and refactoring

--- tools/poi-stations/generate-poi-station-json.go
+++ tools/poi-stations/generate-poi-station-json.go
@@ -7,8 +7,8 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type Station struct {
@@ -24,30 +24,36 @@ type Station struct {
 
 func main() {
 	downloadFiles()
-	generateHAJson()
-	generateNAJson()
+	generateJson("./ha.xls", "HA")
+	generateJson("./na.xls", "NA")
 }
 
 func checkFormat(row []string, format string) bool {
-	expected := strings.Split(format,",")
+	expected := strings.Split(format, ",")
 
 	if len(expected) != len(row) {
 		fmt.Println("unknown format: number of fields does not match")
 		return false
 	}
 
-	for i:= 0; i < len(expected); i++ {
+	for i := 0; i < len(expected); i++ {
 		if expected[i] != row[i] {
-			fmt.Println("unknown format: field does not match:",i,expected[i],row[i])
+			fmt.Println("unknown format: field does not match:", i, expected[i], row[i])
 			return false
 		}
 	}
 	return true
 }
 
-func generateHAJson() {
-	haFormat := "ID,Stations-Name,WMO-Kennung,BG,BM,BS,LG,LM,LS,GEOGR_BREITE,GEOGR_LAENGE,STATIONSHOEHE,Betreiber,Melde-Grp,Country"
-	xls, err := xls.Open("./ha.xls", "utf-8")
+func generateJson(filename string, format string) {
+	header := ""
+	if format == "HA" {
+		header = "ID,Stations-Name,WMO-Kennung,BG,BM,BS,LG,LM,LS,GEOGR_BREITE,GEOGR_LAENGE,STATIONSHOEHE,Betreiber,Melde-Grp,Country"
+	} else if format == "NA" {
+		header = "STATIONSKENNUNG,STATIONSNAME,STATIONS_ID,MaxvonGERAETETYP_NAME,MinvonVON_DATUM,GEOGR_BREITE,GEOGR_LAENGE,STATIONSHOEHE,Niederschlag 1 Min,Schnee manuell,Wind 10 Min,Temperatur und Feuchte 2 m 10 Min,Sonne 10 Min,Erdbodentemperaturen Standard 10 Min,HEADING_BUFR1,HEADING_BUFR2,HEADING_BUFR3,HEADING_BUFR4,HEADING_BUFR5,HEADING_BUFR6,HEADING_BUFR7"
+	}
+
+	xls, err := xls.Open(filename, "utf-8")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -55,54 +61,42 @@ func generateHAJson() {
 
 	all := xls.ReadAllCells(1000000)
 	headlines := all[0]
-	if !checkFormat(headlines, haFormat) {
-		panic("Unknown format")
-	}
-	for i := 1; i < len(all); i++ {
-		row := all[i]
-		id, _ := strconv.Atoi(row[0])
-		name := row[1]
-		kennung := row[2]
-		lat, _ := strconv.ParseFloat(row[9], 64)
-		lon, _ := strconv.ParseFloat(row[10], 64)
-		height, _ := strconv.ParseFloat(row[11], 64)
-		owner := row[12]
-		country := row[14]
-		s := Station{id, name, kennung, lat, lon, height, owner, country}
-		b, _ := json.Marshal(s)
-		fmt.Println(string(b))
-	}
-}
-
-func generateNAJson() {
-	naFormat := "STATIONSKENNUNG,STATIONSNAME,STATIONS_ID,MaxvonGERAETETYP_NAME,MinvonVON_DATUM,GEOGR_BREITE,GEOGR_LAENGE,STATIONSHOEHE,Niederschlag 1 Min,Schnee manuell,Wind 10 Min,Temperatur und Feuchte 2 m 10 Min,Sonne 10 Min,Erdbodentemperaturen Standard 10 Min,HEADING_BUFR1,HEADING_BUFR2,HEADING_BUFR3,HEADING_BUFR4,HEADING_BUFR5,HEADING_BUFR6,HEADING_BUFR7"
-	xls, err := xls.Open("./na.xls", "utf-8")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	all := xls.ReadAllCells(1000000)
-	headlines := all[0]
-	if !checkFormat(headlines, naFormat) {
+	if !checkFormat(headlines, header) {
 		panic("Unknown format")
 	}
 
-	for i := 1; i < len(all); i++ {
-		row := all[i]
-		id, _ := strconv.Atoi(row[2])
-		name := row[1]
-		kennung := row[0]
-		lat, _ := strconv.ParseFloat(row[5], 64)
-		lon, _ := strconv.ParseFloat(row[6], 64)
-		height, _ := strconv.ParseFloat(row[7], 64)
-		owner := ""
-		country := ""
-		s := Station{id, name, kennung, lat, lon, height, owner, country}
-		b,_ := json.Marshal(s)
-		fmt.Println(string(b))
-	}
+	if format == "HA" {
+		for i := 1; i < len(all); i++ {
+			row := all[i]
+			id, _ := strconv.Atoi(row[0])
+			name := row[1]
+			kennung := row[2]
+			lat, _ := strconv.ParseFloat(row[9], 64)
+			lon, _ := strconv.ParseFloat(row[10], 64)
+			height, _ := strconv.ParseFloat(row[11], 64)
+			owner := row[12]
+			country := row[14]
+			s := Station{id, name, kennung, lat, lon, height, owner, country}
+			b, _ := json.Marshal(s)
+			fmt.Println(string(b))
+		}
+	} else if format == "NA" {
+		for i := 1; i < len(all); i++ {
+			row := all[i]
+			id, _ := strconv.Atoi(row[2])
+			name := row[1]
+			kennung := row[0]
+			lat, _ := strconv.ParseFloat(row[5], 64)
+			lon, _ := strconv.ParseFloat(row[6], 64)
+			height, _ := strconv.ParseFloat(row[7], 64)
+			owner := ""
+			country := ""
+			s := Station{id, name, kennung, lat, lon, height, owner, country}
+			b, _ := json.Marshal(s)
+			fmt.Println(string(b))
+		}
 
+	}
 }
 
 func fileExists(path string) (bool, error) {
