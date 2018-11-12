01731a6d7ee029f86e59a013f39081192a4fe262
Martin Leuschner <martinleuschner@googlemail.com>
2018-11-12 21:04:52 +0100

    cleanup

--- cmd/poi-weather/main.go
+++ cmd/poi-weather/main.go
@@ -7,9 +7,17 @@ import (
 
 func main() {
 	poi.LoadDB("../../res/stations.json")
-	_, weather := poi.GetWeatherByName("Hamburg")
-	fmt.Println(".")
-	if weather != nil {
-	}
-	//fmt.Println(weather)
+	station, weather := poi.GetWeatherByName("Hamburg")
+	fmt.Println("Station              :", station.Name)
+	fmt.Println("Time                 :", weather["Parameter description"].Value)
+	fmt.Println("Pressure             :", weather["pressure_reduced_to_mean_sea_level"].Value)
+	fmt.Println("Wind Direction (°)   :", weather["mean_wind_direction_during_last_10 min_at_10_meters_above_ground"].Value)
+	fmt.Println("Wind Speed (km/h)    :", weather["mean_wind_speed_during last_10_min_at_10_meters_above_ground"].Value)
+	fmt.Println("Max Wind Speed (km/h):", weather["maximum_wind_speed_last_hour"].Value)
+	fmt.Println("Temp (°C)            :", weather["dry_bulb_temperature_at_2_meter_above_ground"].Value)
+	fmt.Println("Visibility (km)      :", weather["horizontal_visibility"].Value)
+	fmt.Println("Cloud coverage (%)   :", weather["cloud_cover_total"].Value)
+	fmt.Println("Humidity             :", weather["relative_humidity"].Value)
+	fmt.Println("Cloud height         :", weather["height_of_base_of_lowest_cloud_above_station"].Value)
+	fmt.Println(weather)
 }
